a91f767b0fbea15933592a1311ce159c5459caee
Isaias Del Mundo <idelmundo68@gmail.com>
2020-06-28 21:38:51 -0700

    changed the whole thing

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const fetchInfo = (city = false) => {
-    state.city = city
+// API key 
+var APIKey = "e8baafedb4333fd54c93edc9a7b634f4";
+$("#button").click(function(event) {
+    event.preventDefault();
+    newCity = $("#text").val().trim();
+    console.log(newCity);
 
-    // API
-    const openWeathermapURL = "https://home.openweathermap.org/api_keys";
 
+    // Ajax and search begins
+    const quaryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + newCity + "&units=imperial&appid=" + APIKey;
 
-}
+    $.ajax({
+        url: quaryURL,
+        method: "GET"
+    }).then(function(response) {
+        // log the queryURL
+        // console.log(quaryURL);
+        // log the result object 
+        console.log(response);
+        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+
+        var windSpeed = response.wind.speed;
+        $(".wind").html("<p>Wind Speed: " + windSpeed + "</p>");
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".temp").text("Temperature (F) " + response.main.temp);
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + response.main.temp);
+
+    })
+
+})
